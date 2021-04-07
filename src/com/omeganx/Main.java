82b986f31dafdb8e932e830ad5f2d529b9e24ffd
Omeganx <laville.tom16@gmail.com>
2021-04-07 13:18:11 +0200

    code tweaking

--- src/com/omeganx/Main.java
+++ src/com/omeganx/Main.java
@@ -3,32 +3,30 @@ package com.omeganx;
 import com.omeganx.delaunay.DelaunayHandler;
 import com.omeganx.delaunay.Point;
 import com.omeganx.delaunay.Triangle;
-import com.omeganx.delaunay.TriangleEdge;
 import processing.core.PApplet;
-import com.omeganx.image.*;
 import java.util.ArrayList;
 
 public class Main extends PApplet{
 
-    int n = 500;
+    int width = 500;
+    int height = 500;
     DelaunayHandler del;
-    ArrayList<Point> points;
+
     public void settings(){
-        size(500, 500);
-        Image res = new Image(500, 500);
-        points = res.genPoints(n, 7);
 
-        del = new DelaunayHandler(500, 500);
+        size(width, height);
+        smooth(8);
 
+        del = new DelaunayHandler(width, height);
     }
 
     public void draw(){
-        background(200);
+
+        background(255);
         ArrayList<Triangle> triangles = del.getTriangles();
 
         for(Triangle t : triangles) for(int i = 0 ; i<3; i++)
-                line(t.points[i].x, t.points[i].y, t.points[(i+1)%3].x, t.points[(i+1)%3].y);
-            
+            line(t.points[i].x, t.points[i].y, t.points[(i+1)%3].x, t.points[(i+1)%3].y);
     }
 
     public void mousePressed(){
